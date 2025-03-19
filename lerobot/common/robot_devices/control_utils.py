b52725741bbebb768ba6b8a583cafd8e367eac16
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-03-19 10:20:14 +0100

    Making the change back for open cv test

--- lerobot/common/robot_devices/control_utils.py
+++ lerobot/common/robot_devices/control_utils.py
@@ -268,11 +268,8 @@ def control_loop(
             image_keys = [key for key in observation if "image" in key]
             for key in image_keys:
                 img = observation[key].numpy()
-                print(img.shape, img.dtype)  # Debugging output
-                cv2.namedWindow(key, cv2.WINDOW_NORMAL)  # Allow window resizing
                 cv2.imshow(key, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-                # cv2.moveWindow(key, 100, 100)
-            cv2.waitKey(0)
+            cv2.waitKey(1)
 
         if fps is not None:
             dt_s = time.perf_counter() - start_loop_t
